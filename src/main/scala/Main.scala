e57d95e954212b1d6e942bb6f9e823382fc0cfcd
Cliff Allderige <ca.allderige@gmail.com>
2019-04-08 12:55:42 -0400

    Add getting McDonald's food items.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,8 @@
 object Main extends App {
-
+  var sqlConnection : SqlConnection = new SqlConnection()
+  var foodArrayBuffer = sqlConnection.getCaloriesPerDollar
+  val sortedFood = foodArrayBuffer.sortBy(_.caloriesPerDollar)
+  for (f <- sortedFood) {
+    println("Food item: " + f.item + ", calories per dollar = " + f.caloriesPerDollar)
+  }
 }
--- src/main/scala/SqlConnection.scala
+++ src/main/scala/SqlConnection.scala
@@ -1,3 +1,41 @@
-class SqlConnection (url: String, driver: String, username: String, password: String) {
+import java.sql.{Connection, DriverManager}
+import scala.collection.mutable.ArrayBuffer
 
+class SqlConnection (url: String, driver: String, username: String, password: String) {
+  def this() {
+    this("jdbc:mysql://localhost:3306/db", "com.mysql.jdbc.Driver", "root", "")
+  }
+
+  class FoodItem {
+    var item : String = _
+    var calories : Int = _
+    var price : Double = _
+    var caloriesPerDollar : Double = _
+  }
+
+  def getCaloriesPerDollar: ArrayBuffer[FoodItem] = {
+    var connection : Connection = null
+    var allFood = ArrayBuffer[FoodItem]()
+
+    try {
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+      val statement = connection.createStatement()
+      val rs = statement.executeQuery("SELECT * FROM mcdonalds_food")
+
+      while (rs.next) {
+        var f = new FoodItem
+        f.item = rs.getString("item")
+        f.calories = rs.getInt("calories")
+        f.price = rs.getDouble("price")
+        f.caloriesPerDollar = BigDecimal(f.calories/f.price).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
+        allFood += f
+      }
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+
+    connection.close()
+    allFood
+  }
 }
